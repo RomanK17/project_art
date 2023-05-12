58893811ab448f0838c90221c465b0584d97b1ce
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-12 20:09:33 +0300

    add slider and autoplay

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,20 @@
 import modals from "./modules/modals";
+import sliders from "./modules/sliders";
 
 window.addEventListener("DOMContentLoaded", () => {
   modals();
+
+  sliders({
+    slides: ".feedback-slider-item",
+    sliderDir: "",
+    prevSlideBtn: ".main-prev-btn",
+    nextSlideBtn: ".main-next-btn",
+  });
+
+  sliders({
+    slides: ".main-slider-item",
+    sliderDir: "vertical",
+    prevSlideBtn: "",
+    nextSlideBtn: "",
+  });
 });
---
+++ src/js/modules/sliders.ts
@@ -0,0 +1,83 @@
+const sliders = ({
+  slides,
+  sliderDir,
+  prevSlideBtn,
+  nextSlideBtn,
+}: {
+  slides: string;
+  sliderDir: string;
+  prevSlideBtn: string;
+  nextSlideBtn: string;
+}) => {
+  let pause: boolean = false;
+  let slideIndex: number = 1;
+  const sliderImages = document.querySelectorAll(slides);
+
+  const showSlide = (index: number) => {
+    if (index > sliderImages.length) slideIndex = 1;
+    if (index <= 0) slideIndex = sliderImages.length;
+
+    sliderImages.forEach((slideImage) => {
+      slideImage.classList.add("animated");
+      slideImage.setAttribute("style", "display : none;");
+    });
+
+    (sliderImages[slideIndex - 1] as HTMLElement).style.display = "block";
+  };
+
+  showSlide(slideIndex);
+
+  const changeSlide = (direction: number) => {
+    showSlide((slideIndex += direction));
+  };
+
+  try {
+    const prevBtn = document.querySelector(prevSlideBtn);
+    const nextBtn = document.querySelector(nextSlideBtn);
+
+    if (prevBtn) {
+      prevBtn.addEventListener("click", () => {
+        changeSlide(-1);
+        sliderImages[slideIndex - 1].classList.remove("slideInRight");
+        sliderImages[slideIndex - 1].classList.add("slideInLeft");
+      });
+    }
+
+    if (nextBtn) {
+      nextBtn.addEventListener("click", () => {
+        changeSlide(1);
+        sliderImages[slideIndex - 1].classList.remove("slideInLeft");
+        sliderImages[slideIndex - 1].classList.add("slideInRight");
+      });
+    }
+  } catch (error) {
+    console.log("ошибка при переключении слайдов");
+  }
+
+  const activateAnimation = () => {
+    if (sliderDir === "vertical") {
+      pause = setInterval(() => {
+        changeSlide(1);
+        sliderImages[slideIndex - 1].classList.add("slideInDown");
+      }, 3000);
+    } else {
+      pause = setInterval(() => {
+        changeSlide(1);
+        sliderImages[slideIndex - 1].classList.remove("slideInLeft");
+        sliderImages[slideIndex - 1].classList.add("slideInRight");
+      }, 3000);
+    }
+  };
+
+  activateAnimation();
+
+  sliderImages[0].parentNode.addEventListener("mouseenter", () => {
+    clearInterval(pause);
+  });
+
+  sliderImages[0].parentNode.addEventListener("mouseleave", () => {
+    activateAnimation();
+  });
+};
+
+export default sliders;
