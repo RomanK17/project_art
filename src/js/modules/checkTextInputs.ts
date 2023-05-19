c2d54f363cbc8a8acc52a4470b355e5ad1bddf97
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-19 10:03:57 +0300

    fix bugs

--- src/js/modules/checkTextInputs.ts
+++ src/js/modules/checkTextInputs.ts
@@ -1,9 +1,13 @@
-const checkTextInputs = (selector) => {
+const checkTextInputs = (selector: string) => {
   const textInputs = document.querySelectorAll(selector);
 
+  const isKeyboardEvent = (event: Event): event is KeyboardEvent => {
+    return "key" in event;
+  };
+
   textInputs.forEach((input) => {
-    input.addEventListener("keypress", function (event) {
-      if (event.key.match(/[^а-яё 0-9]/gi)) {
+    input.addEventListener("keypress", function (event: Event) {
+      if (isKeyboardEvent(event) && event.key.match(/[^а-яё 0-9]/gi)) {
         event.preventDefault();
       }
     });
