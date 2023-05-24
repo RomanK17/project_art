81b1387097283300815501c22ce0fb16f1ff333f
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-24 14:17:27 +0300

    optimize code

--- src/js/modules/createCalc.ts
+++ src/js/modules/createCalc.ts
@@ -4,33 +4,52 @@ const createCalc = ({
   optionsSelector,
   promocodeSelector,
   resultSelector,
+}: {
+  sizeSelector: string;
+  materialsSelector: string;
+  optionsSelector: string;
+  promocodeSelector: string;
+  resultSelector: string;
 }): void => {
-  const sizeBlock = document.querySelector(sizeSelector);
-  const materialsBlock = document.querySelector(materialsSelector);
-  const optionsBlock = document.querySelector(optionsSelector);
-  const promocodeBlock = document.querySelector(promocodeSelector);
-  const resultBlock = document.querySelector(resultSelector);
+  const sizeBlock: HTMLOptionElement | null =
+    document.querySelector(sizeSelector);
+  const materialsBlock: HTMLOptionElement | null =
+    document.querySelector(materialsSelector);
+  const optionsBlock: HTMLOptionElement | null =
+    document.querySelector(optionsSelector);
+  const promocodeBlock: HTMLInputElement | null =
+    document.querySelector(promocodeSelector);
+  const resultBlock: HTMLDivElement | null =
+    document.querySelector(resultSelector);
 
-  let sum = 0;
+  if (
+    sizeBlock &&
+    materialsBlock &&
+    optionsBlock &&
+    promocodeBlock &&
+    resultBlock
+  ) {
+    let sum = 0;
 
-  const calcResult = (): void => {
-    sum = Math.round(
-      +sizeBlock.value * +materialsBlock.value + +optionsBlock.value
-    );
+    const calcResult = (): void => {
+      sum = Math.round(
+        +sizeBlock.value * +materialsBlock.value + +optionsBlock.value
+      );
 
-    if (sizeBlock.value == "" || materialsBlock.value == "") {
-      resultBlock.innerHTML = "Выберит размер и материал картины";
-    } else if (promocodeBlock.value === "IWANTPOPART") {
-      resultBlock.innerHTML = Math.round(sum * 0.7);
-    } else {
-      resultBlock.innerHTML = sum;
-    }
-  };
+      if (sizeBlock.value == "" || materialsBlock.value == "") {
+        resultBlock.innerHTML = "Выберит размер и материал картины";
+      } else if (promocodeBlock.value === "IWANTPOPART") {
+        resultBlock.innerHTML = Math.round(sum * 0.7).toString();
+      } else {
+        resultBlock.innerHTML = sum.toString();
+      }
+    };
 
-  sizeBlock.addEventListener("change", calcResult);
-  materialsBlock.addEventListener("change", calcResult);
-  optionsBlock.addEventListener("change", calcResult);
-  promocodeBlock.addEventListener("input", calcResult);
+    sizeBlock.addEventListener("change", calcResult);
+    materialsBlock.addEventListener("change", calcResult);
+    optionsBlock.addEventListener("change", calcResult);
+    promocodeBlock.addEventListener("input", calcResult);
+  }
 };
 
 export default createCalc;
