0b47f8f35e4c53f1e3c3bf8ea0d6bcdd8e291403
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-25 13:51:15 +0300

    optimize code

--- src/js/modules/filterImages.ts
+++ src/js/modules/filterImages.ts
@@ -3,31 +3,34 @@ const filterImages = () => {
   const portfolioWrapper = document.querySelector(".portfolio-wrapper");
   if (menu && portfolioWrapper) {
     const allBtns = menu.querySelectorAll("li");
-    const allWorksBtn = menu.querySelector(".all");
-    const loversBtn = menu.querySelector(".lovers");
-    const chefBtn = menu.querySelector(".chef");
-    const girlBtn = menu.querySelector(".girl");
-    const guyBtn = menu.querySelector(".guy");
-    const grandmotherBtn = menu.querySelector(".grandmother");
-    const granddadBtn = menu.querySelector(".granddad");
-
     const allWorksImgs = portfolioWrapper.querySelectorAll(".all");
     const loversImgs = portfolioWrapper.querySelectorAll(".lovers");
     const chefImgs = portfolioWrapper.querySelectorAll(".chef");
     const girlImgs = portfolioWrapper.querySelectorAll(".girl");
     const guyImgs = portfolioWrapper.querySelectorAll(".guy");
+    const noImgs: HTMLElement | null = document.querySelector(".portfolio-no");
 
-    const noImgs = document.querySelector(".portfolio-no");
+    if (!menu || !allBtns || !allWorksImgs || !noImgs) return;
+
+    const imagesMap = new Map([
+      [allBtns[0], allWorksImgs],
+      [allBtns[1], loversImgs],
+      [allBtns[2], chefImgs],
+      [allBtns[3], girlImgs],
+      [allBtns[4], guyImgs],
+      [allBtns[5], undefined],
+      [allBtns[6], undefined],
+    ]);
 
     const filterByImages = (images: NodeListOf<Element> | undefined) => {
       allWorksImgs.forEach((image) => {
-        image.style.display = "none";
+        (image as HTMLElement).style.display = "none";
         image.classList.remove("animated", "fadeIn");
       });
 
       if (images) {
         images.forEach((image) => {
-          image.style.display = "block";
+          (image as HTMLElement).style.display = "block";
           image.classList.add("animated", "fadeIn");
         });
       } else {
@@ -37,37 +40,12 @@ const filterImages = () => {
     };
 
     menu.addEventListener("click", (event) => {
-      const target = event.target;
+      const target = event.target as HTMLLIElement;
 
       if (target && target.tagName == "LI") {
-        allBtns.forEach((btn) => {
-          btn.classList.remove("active");
-          target.classList.add("active");
-        });
-      }
-
-      switch (target) {
-        case allWorksBtn:
-          filterByImages(allWorksImgs);
-          break;
-        case loversBtn:
-          filterByImages(loversImgs);
-          break;
-        case chefBtn:
-          filterByImages(chefImgs);
-          break;
-        case girlBtn:
-          filterByImages(girlImgs);
-          break;
-        case guyBtn:
-          filterByImages(guyImgs);
-          break;
-        case grandmotherBtn:
-        case granddadBtn:
-          filterByImages(undefined);
-          break;
-        default:
-          console.error("Invalid button clicked");
+        allBtns.forEach((btn) => btn.classList.remove("active"));
+        target.classList.add("active");
+        filterByImages(imagesMap.get(target));
       }
     });
   }
