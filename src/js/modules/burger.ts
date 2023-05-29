2387e0f579621875a81884e6bc89bd418fe32616
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-29 09:13:17 +0300

    add resize event

--- src/js/modules/burger.ts
+++ src/js/modules/burger.ts
@@ -1,17 +1,25 @@
 const createBurger = (menuSelector: string, triggerSelector: string) => {
-  const menuElement = document.querySelector(menuSelector);
-  const burgerElement = document.querySelector(triggerSelector);
+  const menuElement: HTMLUListElement | null =
+    document.querySelector(menuSelector);
+  const burgerElement: HTMLButtonElement | null =
+    document.querySelector(triggerSelector);
 
   if (menuElement && burgerElement) {
     menuElement.style.display = "none";
 
     burgerElement.addEventListener("click", () => {
       if (
-        (menuElement.style.display = "none" && window.screen.availWidth < 993)
+        menuElement.style.display == "none" &&
+        window.screen.availWidth < 993
       ) {
         menuElement.style.display = "block";
       } else {
+        menuElement.style.display = "none";
       }
+    });
+
+    window.addEventListener("resize", () => {
+      if (window.screen.availWidth > 992) menuElement.style.display = "none";
     });
   }
 };
